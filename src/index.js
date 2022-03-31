c4914fd8a727546c41128cfce14f8b84eaa3f362
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-03-31 22:40:45 +0900

    Fix: axiosインストール

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const domain = process.env.REACT_APP_AUTH0_DOMAIN || '';
 const clientId = process.env.REACT_APP_AUTH0_CLIENT_ID || '';
+const audience = process.env.REACT_APP_AUTH0_AUDIENCE || '';
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container);
 
@@ -14,6 +15,7 @@ root.render(
   <Auth0Provider
     domain={domain}
     clientId={clientId}
+    audience={audience}
     redirectUri={window.location.origin}
   >
     <App />
