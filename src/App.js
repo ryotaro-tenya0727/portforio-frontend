06100fda040b00680f9acba5509932310f54af39
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-04-04 06:49:47 +0900

    Add:authoを用いた認証機能実装

--- src/App.js
+++ src/App.js
@@ -1,10 +1,54 @@
-import './App.css';
+import { useEffect, useState } from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
+import axios from 'axios';
+import './App.css';
+
+const postIndex = `${process.env.REACT_APP_REST_URL}/posts`;
 
 function App() {
-  const { user, isAuthenticated, loginWithRedirect, logout } = useAuth0();
-  // 必要な機能をインポート
-  console.log(user);
+  const {
+    user,
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const [token, setToken] = useState('');
+  // 追加
+  const [posts, setPosts] = useState([]);
+  const headers = {
+    headers: {
+      Authorization: token,
+      'Content-Type': 'application/json',
+    },
+  };
+  // 追加
+  const fetchPosts = () => {
+    axios.get(postIndex, headers).then((res) => {
+      setPosts(res.data);
+    });
+  };
+  // 追加
+  const createPosts = () => {
+    const data = {
+      title: 'タイトル4',
+      caption: '説明4',
+    };
+    axios.post('http://localhost:3000/api/v1/posts', data, headers);
+  };
+  useEffect(() => {
+    const getToken = async () => {
+      try {
+        const accessToken = await getAccessTokenSilently({});
+        setToken(accessToken);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    getToken();
+  }, [getAccessTokenSilently]);
+
   return (
     <div className='App'>
       <div style={{ padding: '20px' }}>
@@ -14,6 +58,16 @@ function App() {
         <button onClick={() => logout()}>ログアウト</button>
         <h2>ログイン状態</h2>
         {isAuthenticated ? <p>{user.name}</p> : <p> ログアウト</p>}
+        <h2>投稿作成</h2>
+        <button onClick={createPosts}>投稿作成</button>
+        <h2>投稿一覧</h2>
+        <button onClick={fetchPosts}>投稿取得</button>
+        {posts?.map((post, index) => (
+          <div key={index}>
+            <p>{post.title}</p>
+            <p>{post.caption}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
