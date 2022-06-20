3beaf8baa04144d0eb393cea2630202d1bc6d515
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-06-20 21:51:30 +0900

    Add: 日記にページネーション機能を実装

--- src/components/templates/RecommendedMemberDiariesList.jsx
+++ src/components/templates/RecommendedMemberDiariesList.jsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Grid from '@mui/material/Grid';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
+import SavedSearchIcon from '@mui/icons-material/SavedSearch';
 
 import { useRecommendedMemberDiariesApi } from './../../hooks/useRecommendedMemberDiaries';
+import { usePagination } from './../../hooks/usePagination';
 import { LoginUserDiaryCard } from './../organisms/Organisms';
 import list from './../../css/templates/list.module.css';
 
@@ -20,6 +27,20 @@ const RecommendedMemberDiariesList = ({
     isLoading,
   } = useGetRecommendedMemberDiaries(recommendedMemberId);
 
+  // ページネーション
+  let [page, setPage] = useState(1);
+  const PER_PAGE = 2;
+  let data =
+    recommendedMemberDiaries === undefined
+      ? { length: 0 }
+      : recommendedMemberDiaries.data;
+  const count = Math.ceil(data.length / PER_PAGE);
+  const _DATA = usePagination(data, PER_PAGE);
+  const handleChange = (_e, p) => {
+    setPage(p);
+    _DATA.jump(p);
+  };
+
   const theme = createTheme({
     breakpoints: {
       values: {
@@ -28,6 +49,14 @@ const RecommendedMemberDiariesList = ({
         md: 1290,
         lg: 1400,
         xl: 1536,
+      },
+    },
+    palette: {
+      primary: {
+        main: '#ff96df',
+      },
+      secondary: {
+        main: '#DC143C',
       },
     },
   });
@@ -40,24 +69,46 @@ const RecommendedMemberDiariesList = ({
         {isIdle || isLoading ? (
           <h2>日記ローディング中</h2>
         ) : (
-          <Grid container spacing={3}>
-            {recommendedMemberDiaries.data.map((diary, index) => {
-              return (
-                <LoginUserDiaryCard
-                  key={index}
-                  diaryId={diary.attributes.id}
-                  eventName={diary.attributes.event_name}
-                  eventDate={diary.attributes.event_date}
-                  eventVenue={diary.attributes.event_venue}
-                  status={diary.attributes.status}
-                  diaryImages={diary.attributes.diary_images}
-                  eventPolaroidCount={diary.attributes.event_polaroid_count}
-                  showUrl={`/recommended-member/${recommendedMemberUuid}/diaries/show/${diary.attributes.id}?recommended_member_nickname=${recommendedMemberNickname}&group=${recommendedMemberGroup}`}
-                  editUrl={`/recommended-member/${recommendedMemberUuid}/diaries/${recommendedMemberId}/edit/${diary.attributes.id}?nickname=${recommendedMemberNickname}&group=${recommendedMemberGroup}`}
-                />
-              );
-            })}
-          </Grid>
+          <>
+            <div className={list.pagination_and_search_wrap}>
+              <Pagination
+                sx={{ margin: '0 auto' }}
+                color='primary'
+                size='large'
+                className={list.pagination}
+                count={count}
+                page={page}
+                renderItem={(item) => (
+                  <PaginationItem
+                    components={{
+                      previous: ArrowBackIcon,
+                      next: ArrowForwardIcon,
+                    }}
+                    {...item}
+                  />
+                )}
+                onChange={handleChange}
+              />
+            </div>
+            <Grid container spacing={3}>
+              {_DATA.currentData().map((diary, index) => {
+                return (
+                  <LoginUserDiaryCard
+                    key={index}
+                    diaryId={diary.attributes.id}
+                    eventName={diary.attributes.event_name}
+                    eventDate={diary.attributes.event_date}
+                    eventVenue={diary.attributes.event_venue}
+                    status={diary.attributes.status}
+                    diaryImages={diary.attributes.diary_images}
+                    eventPolaroidCount={diary.attributes.event_polaroid_count}
+                    showUrl={`/recommended-member/${recommendedMemberUuid}/diaries/show/${diary.attributes.id}?recommended_member_nickname=${recommendedMemberNickname}&group=${recommendedMemberGroup}`}
+                    editUrl={`/recommended-member/${recommendedMemberUuid}/diaries/${recommendedMemberId}/edit/${diary.attributes.id}?nickname=${recommendedMemberNickname}&group=${recommendedMemberGroup}`}
+                  />
+                );
+              })}
+            </Grid>
+          </>
         )}
       </ThemeProvider>
     </div>
