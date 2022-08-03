517bb96514a220b8930947d4ef779a1eacfb56f0
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-08-03 20:40:23 +0900

    Fix: スマホ対応修正

--- src/components/molecules/LoginName.jsx
+++ src/components/molecules/LoginName.jsx
@@ -1,7 +1,7 @@
 import layout from './../../css/layout/layout.module.css';
 
 const LoginName = ({ name }) => {
-  return <p>{name}さんログイン中</p>;
+  return <p className={layout.login_name}>{name}さんログイン中</p>;
 };
 
 export default LoginName;
