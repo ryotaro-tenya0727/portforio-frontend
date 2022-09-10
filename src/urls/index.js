92745ff1bc2a6aa0ef1731369b190e892474911a
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-11 08:48:37 +0900

    Change: ログイン情報のAPIのルート変更

--- src/urls/index.js
+++ src/urls/index.js
@@ -2,7 +2,7 @@ export const REST_API_URL = `${process.env.REACT_APP_REST_URL}/api/v1`;
 export const API_URL = `${process.env.REACT_APP_REST_URL}`;
 
 export const registerUserUrl = `${REST_API_URL}/users`;
-export const getUserUrl = `${REST_API_URL}/users`;
+export const getUserUrl = `${REST_API_URL}/user/users`;
 export const recommendedMembersIndexUrl = `${REST_API_URL}/user/recommended_members`;
 export const recommendedMembersCreateUrl = `${REST_API_URL}/user/recommended_members`;
 export const recommendedMembersPatchUrl = `${REST_API_URL}/user/recommended_members/:id`;
