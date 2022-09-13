f8be0d45b2283428f42cfb955c78097aa4d91fda
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-09-13 20:16:07 +0900

    ADD: タイムラインの無限スクロール実装

--- src/components/templates/TimeLineList.jsx
+++ src/components/templates/TimeLineList.jsx
@@ -5,6 +5,7 @@ import { useQuery } from 'react-query';
 import { useContext } from 'react';
 import { AuthGuardContext } from './../../providers/AuthGuard';
 import CircularProgress from '@mui/material/CircularProgress';
+import InfiniteScroll from 'react-infinite-scroller';
 
 import useMedia from 'use-media';
 
@@ -14,16 +15,17 @@ import { API_URL } from '../../urls/index';
 const TimeLineList = ({ isAuthenticated }) => {
   const isWide = useMedia({ minWidth: '700px' });
   const [diaries, setDiaries] = useState([]);
-  const { setAccessToken } = useContext(AuthGuardContext);
+  const { accessToken, setAccessToken } = useContext(AuthGuardContext);
   const { getAccessTokenSilently } = useAuth0();
-  const [isLoading, setIsLoading] = useState(false);
-  let { isLoading: queryLoading, data: all_diaries } = useQuery(
-    ['all_users'],
+  // const [isLoading, setIsLoading] = useState(false);
+  const [hasMore, setHasMore] = useState(true);
+  let { isLoading: queryLoading, data: time_lines } = useQuery(
+    ['time_lines'],
     async () => {
       const token = isAuthenticated ? await getAccessTokenSilently() : null;
       setAccessToken(token);
       const response = await axios
-        .get(`${API_URL}/api/v1/user/timeline`, {
+        .get(`${API_URL}/api/v1/user/timeline?page=1`, {
           headers: {
             Authorization: `Bearer ${token}`,
             'Content-Type': 'application/json',
@@ -32,14 +34,54 @@ const TimeLineList = ({ isAuthenticated }) => {
         .catch((error) => {
           console.error(error.response.data);
         });
-      setDiaries(response.data);
+      return response.data.data;
     },
     {
       cacheTime: 0,
       staleTime: 3000000,
     }
   );
-  if (queryLoading || isLoading) {
+
+  const loadMore = async (page) => {
+    // const response = await fetch(`http://localhost:3000/api/test?page=${page}`); //API通信
+    await axios
+      .get(`${API_URL}/api/v1/user/timeline?page=${page}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((response) => {
+        const data = response.data.data;
+        //データ件数が0件の場合、処理終了
+        if (data.length < 1) {
+          setHasMore(false);
+          return;
+        }
+        setDiaries([...diaries, ...data]);
+      })
+      .catch((error) => {
+        console.error(error.response.data);
+      });
+
+    //取得データをリストに追加
+  };
+  const loader = (
+    <div style={{ textAlign: 'center', marginTop: '150px' }}>
+      <CircularProgress
+        sx={{
+          color: '#ff94df',
+          mt: -0.4,
+          fontSize: '80px',
+          '@media screen and (min-width:700px)': {
+            mt: -1,
+          },
+        }}
+        size={isWide ? 60 : 30}
+      />
+    </div>
+  );
+  if (queryLoading) {
     return (
       <div style={{ textAlign: 'center', marginTop: '150px' }}>
         <CircularProgress
@@ -56,9 +98,10 @@ const TimeLineList = ({ isAuthenticated }) => {
       </div>
     );
   }
+  console.log(time_lines);
   return (
     <>
-      {diaries.data.map((diary, index) => (
+      {time_lines.map((diary, index) => (
         <TimeLineCard
           key={index}
           diaryId={diary.attributes.id}
@@ -74,6 +117,30 @@ const TimeLineList = ({ isAuthenticated }) => {
           showUrl={`/diaries/show/${diary.attributes.id}}`}
         />
       ))}
+      <InfiniteScroll
+        loadMore={loadMore} //項目を読み込む際に処理するコールバック関数
+        hasMore={hasMore} //読み込みを行うかどうかの判定
+        loader={loader}
+        initialLoad={false}
+        pageStart={1}
+      >
+        {diaries.map((diary, index) => (
+          <TimeLineCard
+            key={index}
+            diaryId={diary.attributes.id}
+            diaryUserName={diary.attributes.diary_user_name}
+            diaryUserImage={diary.attributes.diary_user_image}
+            eventName={diary.attributes.event_name}
+            eventDate={diary.attributes.event_date}
+            eventVenue={diary.attributes.event_venue}
+            DiaryMemberNickname={diary.attributes.diary_member_nickname}
+            diaryImage={diary.attributes.diary_image}
+            eventPolaroidCount={diary.attributes.event_polaroid_count}
+            ImpressiveMemory={diary.attributes.impressive_memory}
+            showUrl={`/diaries/show/${diary.attributes.id}}`}
+          />
+        ))}
+      </InfiniteScroll>
     </>
   );
 };
