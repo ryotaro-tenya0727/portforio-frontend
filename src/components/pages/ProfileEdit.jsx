26c1599e24acb98b80bf97f935f7b2ef785e1c8d
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2024-03-31 20:21:14 +0900

    ADD: プロフィール編集画面追加

--- src/components/pages/ProfileEdit.jsx
+++ src/components/pages/ProfileEdit.jsx
@@ -1,8 +1,56 @@
-const ProfileEdit = () => {
+import { memo } from 'react';
+import LoyaltyIcon from '@mui/icons-material/Loyalty';
+
+import { HomeBreadText } from './../atoms/atoms';
+import { BreadCrumbs, Headers } from './../organisms/Organisms';
+import { RecommenedMembersNewForm, Loading } from './../templates/Templates';
+import { RedirectToLogin } from './Pages';
+import { useUsersApi } from './../../hooks/useUsers';
+
+const ProfileEdit = memo(() => {
+  const { isAuthenticated, user, isAuthLoading } = useUsersApi();
+  const breadcrumbs = [
+    {
+      title: (
+        <>
+          <HomeBreadText />
+        </>
+      ),
+      to: '/',
+    },
+    { title: 'マイページ', to: '/mypage' },
+    {
+      title: (
+        <>
+          {' '}
+          <LoyaltyIcon
+            sx={{
+              fontSize: '19px',
+              mb: '-4px',
+              mr: '4px',
+              color: '#ff6fc8',
+            }}
+          />
+          プロフィール編集ページ
+        </>
+      ),
+      to: '/recommended-members/new',
+    },
+  ];
+  if (isAuthLoading) {
+    return <Loading />;
+  }
+  if (isAuthenticated === false) {
+    return <RedirectToLogin />;
+  }
   return (
-    <div>
-      <h1>ProfileEdit</h1>
-    </div>
+    isAuthenticated && (
+      <>
+        <Headers name={user.name} />
+        <BreadCrumbs breadcrumbs={breadcrumbs} />
+        <RecommenedMembersNewForm />
+      </>
+    )
   );
-};
+});
 export default ProfileEdit;
